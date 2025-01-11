42986542f3a74fe712873e292fca0728b4e27902
Andrés Almeida Jara <almeidaandres12@gmail.com>
2025-01-11 00:11:28 -0500

    Delete

--- Microservicios Chidos/micro-cursos/src/main/java/com/espe/micro_cursos/repositories/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.espe.micro_cursos.repositories;
-
-import com.espe.micro_cursos.model.entity.Curso;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CursoRepository extends CrudRepository<Curso, Long> {
-}
--- Microservicios Chidos/micro-estudiantes/src/main/java/com/espe/micro_estudiantes/repositories/EstudianteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.espe.micro_estudiantes.repositories;
-
-import com.espe.micro_estudiantes.model.entity.Estudiante;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EstudianteRepository extends CrudRepository<Estudiante, Long> {
-}
